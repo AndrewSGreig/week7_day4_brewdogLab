9a682f8400e72f8c8356a470ad29c59ded718fda
User <user@Users-MacBook-Pro.local>
2020-02-20 12:11:36 +0000

    now displays images of the beers in a ul

--- brewdog/src/components/BeerList.vue
+++ brewdog/src/components/BeerList.vue
@@ -1,9 +1,14 @@
 <template lang="html">
   <div id="beerList">
     <p> In Beer List </p>
-    {{beers}}
+    <!-- {{beers}} -->
     <ul>
-      <li v-for="(beer, index) in beers"  :key="index" ></li>
+      <li v-for="(beer, index) in beers"  :key="index" >
+        {{beer.name}}
+<div id="right">
+        <img :src="beer.image_url" :alt="beer.name">
+</div>
+      </li>
     </ul>
   </div>
 </template>
@@ -17,4 +22,7 @@
 </script>
 
 <style lang="css" scoped>
+#right img {
+  width: 5%;
+}
 </style>
